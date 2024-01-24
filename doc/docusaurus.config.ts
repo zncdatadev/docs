ee9367dcb2b2bf2d679edcacb443c824ea47cfac
whg517 <huagang517@126.com>
2024-01-24 11:14:51 +0800

    feat: init doc site (#1)
    * feat: init docusaurus project
    
    * feat: init doc struct and config docusaurus
    
    * chore: add gh action
    
    * chore: chnage gh workflows ame
    
    * chore: update gh action trigger condition
    
    * chore: change gh action node ver to 18
    
    * chore(action): add tsc lint

---
+++ doc/docusaurus.config.ts
@@ -0,0 +1,123 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'ZNCData Stack',
+  tagline: 'zncdata stack',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://zncdata-labs.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'ZNCData labs', // Usually your GitHub org/user name.
+  projectName: 'zncdata-stack', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'zh'],
+    path: 'i18n',
+    localeConfigs: {
+      zh: {
+        label: '简体中文',
+        htmlLang: 'zh-cmn-Hans',
+      },
+    },
+  },
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/zncdata-labs/zncdata-stack',
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          includeCurrentVersion: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'ZNCData Stack',
+      logo: {
+        alt: 'zncdata stack',
+        src: 'img/favicon.ico',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'docs',
+          position: 'left',
+          label: 'Docs',
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+        {
+          label: 'Languages',
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Document',
+          items: [
+            {
+              label: 'Installation',
+              to: 'docs/quick-start/installation', // ref sidebars.ts
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/zncdata-labs/zncdata-stack',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} ZNCData labs. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
